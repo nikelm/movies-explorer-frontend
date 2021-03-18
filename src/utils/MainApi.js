f8e39e9b1cef1dd58fa790555a740cae15881a52
Nikolay Elmanov <nikelm@yandex.ru>
2021-03-18 12:47:52 +0300

    fix: назначить base_url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3003';
+export const BASE_URL = 'https://api.mymovies.students.nomoredomains.monster/';
 
 
 export const register = (name, password, email) => {
